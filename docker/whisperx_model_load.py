1002e87bb9cacfc14f0e5e82f63574c509dcfb33
hsuanguo <hsuan.guo@gmail.com>
2024-02-29 22:53:42 +0000

    simplify

--- docker/whisperx_model_load.py
+++ docker/whisperx_model_load.py
@@ -1,14 +1,8 @@
-import argparse
 import torch
-import os
-from args import get_args_parser, MODEL_DIR
 import whisper
 
-parser = argparse.ArgumentParser(parents=[get_args_parser()])
-args = parser.parse_args()
-print(args.model_name)
-args.model_name = os.path.join(os.environ["TRANSFORMERS_CACHE"], args.model_name)
-device = torch.device(args.device)
+MODEL_DIR='./TOFILL'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 print("load Whisper model")
-asr_model = whisper.load_model('large-v2', args.device, download_root=MODEL_DIR)
+asr_model = whisper.load_model('large-v2', device, download_root=MODEL_DIR)
